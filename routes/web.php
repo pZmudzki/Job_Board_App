933109858a12058d0927b051ca6e09ca0eb69f46
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-06-09 17:14:26 +0200

    Configure tailwindcss and debugbar. Create layout component and job index template

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
+use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', '/jobs');
+
+Route::resource('jobs', JobController::class);
