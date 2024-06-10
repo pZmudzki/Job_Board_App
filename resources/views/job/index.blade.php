86bb16606135132a31602f30bf1081f890f389f3
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-06-10 21:11:14 +0200

    Add filter form inputs clearing buttons

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -2,17 +2,20 @@
   <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index'),]" />
 
   <x-card class="mb-4 text-sm">
-    <form action="{{route('jobs.index')}}" method="GET">
+    <form action="{{route('jobs.index')}}" method="GET" id="filtering-form">
       <div class="mb-4 grid grid-cols-2 gap-4">
         <div>
           <div class="mb-1 font-semibold">Search</div>
-          <x-text-input name="search" value="{{request('search')}}" placeholder="Search for any text" />
+          <x-text-input name="search" value="{{request('search')}}" form-id="filtering-form"
+            placeholder="Search for any text" />
         </div>
         <div>
           <div class="mb-1 font-semibold">Salary</div>
           <div class="flex gap-2">
-            <x-text-input name="min_salary" value="{{request('min_salary')}}" placeholder="From" />
-            <x-text-input name="max_salary" value="{{request('max_salary')}}" placeholder="To" />
+            <x-text-input name="min_salary" value="{{request('min_salary')}}" form-id="filtering-form"
+              placeholder="From" />
+            <x-text-input name="max_salary" value="{{request('max_salary')}}" form-id="filtering-form"
+              placeholder="To" />
           </div>
         </div>
         <x-radio-group name='experience' :options="\App\Models\Job::$experience" />
