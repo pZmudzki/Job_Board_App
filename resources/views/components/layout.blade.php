48541d68702fde71af4f4e150ffe675d2911c397
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-06-10 23:53:24 +0200

    Style index page, connect AlpineJS and implement input reset functionality

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <title>Job Board</title>
 </head>
 
-<body class="container mx-auto mt-10 max-w-2xl bg-slate-200 text-slate-700">
+<body
+    class="container mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 text-slate-700">
     {{$slot}}
 </body>
 
--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,27 +1,25 @@
 <x-layout>
   <x-breadcrumbs class="mb-4" :links="['Jobs' => route('jobs.index'),]" />
 
-  <x-card class="mb-4 text-sm">
-    <form action="{{route('jobs.index')}}" method="GET" id="filtering-form">
+  <x-card class="mb-4 text-sm" x-data="">
+    <form x-ref="filters" action="{{route('jobs.index')}}" method="GET" id="filtering-form">
       <div class="mb-4 grid grid-cols-2 gap-4">
         <div>
           <div class="mb-1 font-semibold">Search</div>
-          <x-text-input name="search" value="{{request('search')}}" form-id="filtering-form"
+          <x-text-input name="search" value="{{request('search')}}" form-ref="filters"
             placeholder="Search for any text" />
         </div>
         <div>
           <div class="mb-1 font-semibold">Salary</div>
           <div class="flex gap-2">
-            <x-text-input name="min_salary" value="{{request('min_salary')}}" form-id="filtering-form"
-              placeholder="From" />
-            <x-text-input name="max_salary" value="{{request('max_salary')}}" form-id="filtering-form"
-              placeholder="To" />
+            <x-text-input name="min_salary" value="{{request('min_salary')}}" form-ref="filters" placeholder="From" />
+            <x-text-input name="max_salary" value="{{request('max_salary')}}" form-ref="filters" placeholder="To" />
           </div>
         </div>
         <x-radio-group name='experience' :options="\App\Models\Job::$experience" />
         <x-radio-group name='category' :options="\App\Models\Job::$category" />
       </div>
-      <button class="w-full">Filter</button>
+      <x-button class="w-full">Filter</x-button>
     </form>
   </x-card>
 
