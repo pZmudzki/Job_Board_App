c6f86caa166d755e9a712cb455c55221758971f9
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-06-14 20:57:42 +0200

    Create logging out functionality

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -10,7 +10,31 @@
 </head>
 
 <body class="mx-auto mt-10 max-w-2xl bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 text-slate-700">
-    {{auth()->user()->name ?? 'Guest'}}
+    <nav class="mb-8 flex justify-between text-lg font-medium">
+        <ul class="flex gap-2">
+            <li>
+                <a href="{{route('jobs.index')}}">Home</a>
+            </li>
+        </ul>
+        <ul class="flex gap-2">
+            @auth
+            <li>
+                {{auth()->user()->name ?? 'Anonymus'}}
+            </li>
+            <li>
+                <form action="{{route('auth.destroy')}}" method="POST">
+                    @csrf
+                    @method('DELETE')
+                    <button>Logout</button>
+                </form>
+            </li>
+            @else
+            <li>
+                <a href="{{route('login')}}">Sign in</a>
+            </li>
+            @endauth
+        </ul>
+    </nav>
     {{$slot}}
 </body>
 
