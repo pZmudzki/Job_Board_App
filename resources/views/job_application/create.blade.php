6f19082338e62ca830191c23b8cf27177c2e95be
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-06-19 21:43:13 +0200

    Handle cv uploads and local storage

--- resources/views/job_application/create.blade.php
+++ resources/views/job_application/create.blade.php
@@ -1,25 +1,30 @@
 <x-layout>
-  <x-breadcrumbs class="mb-4" :links="[
-    'Jobs' => route('jobs.index'),
-    $job->title => route('jobs.show', $job->id),
-    'Apply' => '#' 
+    <x-breadcrumbs class="mb-4" :links="[
+        'Jobs' => route('jobs.index'),
+        $job->title => route('jobs.show', $job->id),
+        'Apply' => '#',
     ]" />
 
-  <x-job-card :job="$job" />
+    <x-job-card :job="$job" />
 
-  <x-card>
-    <h2 class="mb-4 text-lg font-medium">
-      Your Job Application
-    </h2>
+    <x-card>
+        <h2 class="mb-4 text-lg font-medium">
+            Your Job Application
+        </h2>
 
-    <form action="{{route('job.application.store', $job->id)}}" method="POST">
-      @csrf
-      <div class="mb-4">
-        <label for="expected_salary" class="mb-2 block text-sm font-medium text-slate-900">Expected Salary</label>
-        <x-text-input type="number" name="expected_salary" />
-      </div>
+        <form action="{{ route('job.application.store', $job->id) }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="mb-4">
+                <label for="expected_salary" class="mb-2 block text-sm font-medium text-slate-900">Expected Salary</label>
+                <x-text-input type="number" name="expected_salary" />
+            </div>
 
-      <x-button class="w-full">Apply</x-button>
-    </form>
-  </x-card>
+            <div class="mb-4">
+                <label for="cv" class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
+                <x-text-input type="file" name="cv" id="cv" />
+            </div>
+
+            <x-button class="w-full">Apply</x-button>
+        </form>
+    </x-card>
 </x-layout>
