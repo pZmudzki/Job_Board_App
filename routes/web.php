639ba25aee764c1027a2ad397d9c3528091546e8
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-06-24 23:20:14 +0200

    Create employer controller and implement employer creation. Add Employer policy and connect it with gate facade

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\JobApplicationController;
+use App\Http\Controllers\EmployerController;
 use App\Http\Controllers\MyJobApplicationController;
 use Illuminate\Support\Facades\Route;
 
@@ -21,4 +22,6 @@ Route::middleware('auth')->group(function () {
   Route::resource('job.application', JobApplicationController::class)->only(['create', 'store']);
 
   Route::resource('my-job-applications', MyJobApplicationController::class)->only(['index', 'destroy']);
+
+  Route::resource('employer', EmployerController::class)->only(['create', 'store']);
 });
