ded96860717de59688bf99ac98b2cca3f381d2fe
Piotr Żmudzki <piotr.zmudzki47@gmail.com>
2024-06-26 15:08:56 +0200

    Show softly deleted jobs to user

--- resources/views/components/job-card.blade.php
+++ resources/views/components/job-card.blade.php
@@ -1,31 +1,34 @@
 <x-card class="mb-4">
     <div class="flex justify-between items-center mb-4">
         <h2 class="text-lg font-medium">
-            {{$job->title}}
+            {{ $job->title }}
         </h2>
         <span class="text-slate-500">
-            ${{number_format($job->salary)}}
+            ${{ number_format($job->salary) }}
         </span>
     </div>
 
     <div class="mb-4 flex justify-between items-center text-sm text-slate-500">
-        <div class="flex gap-4">
-            <div>{{$job->employer->company_name}}</div>
-            <div>{{$job->location}}</div>
+        <div class="flex gap-4 items-center">
+            <div>{{ $job->employer->company_name }}</div>
+            <div>{{ $job->location }}</div>
+            @if ($job->deleted_at)
+                <span class="text-xs text-red-500">Deleted</span>
+            @endif
         </div>
         <div class="flex gap-1 text-xs">
             <x-tag>
-                <a href="{{route('jobs.index', ['experience' => $job->experience])}}">
-                    {{Str::ucfirst($job->experience)}}
+                <a href="{{ route('jobs.index', ['experience' => $job->experience]) }}">
+                    {{ Str::ucfirst($job->experience) }}
                 </a>
             </x-tag>
             <x-tag>
-                <a href="{{route('jobs.index', ['category' => $job->category])}}">
-                    {{$job->category}}
+                <a href="{{ route('jobs.index', ['category' => $job->category]) }}">
+                    {{ $job->category }}
                 </a>
             </x-tag>
         </div>
     </div>
 
-    {{$slot}}
+    {{ $slot }}
 </x-card>
